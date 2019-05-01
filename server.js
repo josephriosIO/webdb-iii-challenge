39310bf49ab816658410bb168ab43b352bb68ff8
josephriosio <blankworldwide@gmail.com>
2019-05-01 14:24:31 -0700

    added new route for students

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 
 const cohortRouter = require("./routes/cohort");
+const studentRouter = require("./routes/students");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
+server.use("/students", studentRouter);
 server.use("/api/cohorts", cohortRouter);
 
 module.exports = server;
