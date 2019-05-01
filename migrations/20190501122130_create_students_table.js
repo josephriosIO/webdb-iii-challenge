0ee71e2b6b7e32bdb866e919bb2b21e29a7decdc
josephriosio <blankworldwide@gmail.com>
2019-05-01 12:41:18 -0700

    created tables and helper db functions

---
+++ migrations/20190501122130_create_students_table.js
@@ -0,0 +1,22 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("students", tbl => {
+    tbl.increments();
+
+    tbl
+      .string("name", 255)
+      .unique()
+      .notNullable();
+
+    tbl
+      .integer("cohort_id")
+      .unsigned()
+      .references("id")
+      .inTable("cohort")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("students");
+};
--- routes/cohort.js
+++ routes/cohort.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
 
+const cohortDb = require("../data/helpers/cohortDb");
+
+router.get("/", async (req, res) => {
+  try {
+    const cohorts = await cohortDb.get();
+    res.status(200).json(cohorts);
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+});
+
 module.exports = router;
